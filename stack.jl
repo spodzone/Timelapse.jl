15a7de5cb2e86e18561c42565314077526105596
Tim Haynes <github@stirfried.vegetable.org.uk>
2018-09-04 01:33:49 +0100

    Major rewrite stack.jl
    Switch to custom tlog()
    Use whole v1.0.0 Images APIs
    Cater for new scope rules in for... loops

--- stack.jl
+++ stack.jl
@@ -1,42 +1,53 @@
 #!/usr/bin/env julia
 
-info("Starting")
+function tlog(str)
+    tm=time()
+    println("[$tm] - $str")
+end
 
-info("Loading modules")
+tlog("Starting")
 
-using Images, FixedPointNumbers
+tlog("Loading modules")
+
+using Images
 
 function imageinterpolate(img1, img2, prop=0.5)
   "Return a new image interpolated PROP-proportion of the way from img1 to img2 (arrays)"
-  d=[ img1[i]+ (img2[i]-img1[i])*prop for i in 1:length(img1) ]
+  d = img1 + (img2-img1)*prop
   d
 end
 
 
 fname=ARGS[1]
-info("Loading image 1: [$fname]")
+tlog("Loading image 1: [$fname]")
 rolling=load(ARGS[1])
-w,h,props = (width(rolling), height(rolling), properties(rolling))
+w,h=size(rolling)
 
-for i in 2:length(ARGS)
-  fname=ARGS[i]
-  info("Loading image $i: [$fname]")
-  img=load(fname)
-  img=reshape(data(float32(img)), w*h)
-  n=imageinterpolate(rolling, img, 1/i)
-  rolling=deepcopy(n)
+for i in 1:length(ARGS)
+    fname=ARGS[i]
+    tlog("Loading image $i [$fname]")
+    img=load(fname)
+    img = RGB{Float16}.(img)    
+    if(i==1)
+        global rolling=deepcopy(img)
+    else
+        n=imageinterpolate(rolling, img, 1/i)
+        global rolling=deepcopy(n)
+    end
+        
 end
 
-info("Converting final format")
+tlog("Converting final format")
 rolling=reshape(rolling, (w,h))
-rolling=convert(Image{Images.RGB24}, rolling)
 
-info("Saving")
+tlog("Saving")
 #info("  EXR")
-#imwrite(rolling, "output.exr")
-info("  PNG")
-imwrite(rolling, "output.png")
-info("  TIFF")
-imwrite(rolling, "output.tiff")
+#save("output.exr", rolling)
+tlog("  PNG")
+rolling=RGB{N0f8}.(rolling)
 
-info("All done")
+save("output.png", rolling)
+tlog("  TIFF")
+save("output.tiff", rolling)
+
+tlog("All done")
