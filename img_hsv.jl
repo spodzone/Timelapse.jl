46a40fe8d28df759f1a9599b032a9aa6c3c102b1
Tim Haynes <github@stirfried.vegetable.org.uk>
2019-08-19 20:49:25 +0000

    Initial addition: img_hsv.jl for extracting LUT cube from image averages

---
+++ img_hsv.jl
@@ -0,0 +1,38 @@
+#!/usr/bin/env julia
+
+@info("Starting")
+
+levels=5
+lvls=levels-1
+sat=0.25
+
+@info("Loading modules")
+
+using Images, Statistics
+
+@info("Loading images")
+imgs=[ imresize(load(x), ratio=0.5) for x in ARGS ]
+
+@info("Joining arrays")
+img=foldr( (a,b)->cat(a,b,dims=1), imgs)
+hsv=HSV{Float32}.(img)
+
+vs=Array(0:1/(levels-1):1)
+
+@info("Calculating")
+hbyv = [ (h.h, round(h.v*lvls)/lvls) for h in hsv ]
+meanhbyv = [ ( mean([ h for (h,v) in hbyv if v==vv]) , vv) for vv in vs ]
+curves=[ (RGB(HSV{Float32}.(h, sat,v )), v) for (h,v) in meanhbyv ]
+
+@info("LUT (3d Cube format)")
+#map(println, curves)
+
+println("TITLE \"test\"\nLUT_3D_SIZE $levels\n")
+println("DOMAIN_MIN 0 0 0\nDOMAIN_MAX 1.0 1.0 1.0 \n")
+for thing in curves
+    r,g,b=thing[1].r, thing[1].g, thing[1].b
+    println("$r $g $b")
+end
+
+
+
