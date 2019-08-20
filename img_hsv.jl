9b25c2cebce58c492bfd5320bd97f202439e33ae
Tim Haynes <github@stirfried.vegetable.org.uk>
2019-08-20 23:34:46 +0000

    Major rewrite: switch to RGB instead of hue at each L bucket
    Also avoids having to assume saturation per value in recalculation

--- img_hsv.jl
+++ img_hsv.jl
@@ -21,19 +21,18 @@ hsv=HSV{Float32}.(img)
 vs=Array(0:1/(levels-1):1)
 
 @info("Calculating")
-hbyv = [ (h.h, round(h.v*lvls)/lvls) for h in hsv ]
+hbyv = [ (RGB(h), round(h.v*lvls)/lvls) for h in hsv ]
 meanhbyv = [ ( mean([ h for (h,v) in hbyv if v==vv]) , vv) for vv in vs ]
-curves=[ (RGB(HSV{Float32}.(h, sat,v )), v) for (h,v) in meanhbyv ]
+curves=zip(meanhbyv, vs)
 
 @info("LUT (3d Cube format)")
-#map(println, curves)
 
 println("TITLE \"test\"\nLUT_3D_SIZE $levels\n")
 println("DOMAIN_MIN 0 0 0\nDOMAIN_MAX 1.0 1.0 1.0 \n")
-for thing in curves
+for (thing,v) in curves
     r,g,b=thing[1].r, thing[1].g, thing[1].b
+    @info("$r $g $b v=$v")
     println("$r $g $b")
 end
-
 
 
